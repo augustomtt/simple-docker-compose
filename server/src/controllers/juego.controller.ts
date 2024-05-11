90576bdbd4c0434236d79e27c814464d4217bbc4
mostri28 <mateoetchepare@gmail.com>
2024-05-11 19:29:57 -0300

    id assignment

--- server/src/controllers/juego.controller.ts
+++ server/src/controllers/juego.controller.ts
@@ -1,7 +1,12 @@
 import { Request, Response } from "express";
 import { db } from "../config/pouchdb.config";
+import { sha1 } from 'object-hash'; // Import only the hash function
+
 
 export async function fetchJuegos(req: Request, res: Response) {
+
+
+
     try {
         const docs = await db.allDocs({
             include_docs: true,
@@ -15,6 +20,9 @@ export async function fetchJuegos(req: Request, res: Response) {
 
 export function guardarNuevoJuego(req: Request, res: Response) {
     const { body } = req;
+    const hash = sha1(body)
+    body.id = hash;
+
     db.put(body).then((result: any) => {
         res.send(result);
     }).catch((err: any) => {
