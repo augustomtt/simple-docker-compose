539d0ca3591210a2557e9b993d716b47d4b31a15
mostri28 <mateoetchepare@gmail.com>
2024-05-11 17:22:33 -0300

    commit intermedio

--- front/src/app/pages/main-page/main-page/main-page.component.ts
+++ front/src/app/pages/main-page/main-page/main-page.component.ts
@@ -2,16 +2,27 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input'
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button'
 
 @Component({
   selector: 'main-page',
   standalone: true,
   imports: [
     CommonModule,
+    MatButtonModule,
     MatFormFieldModule,
     MatInputModule,
+    MatSelectModule,
   ],
   templateUrl: `main-page.component.html`,
 })
-export class MainPageComponent { }
+export class MainPageComponent { 
+  public consolas: Array<string> = ["PC", 
+  "PlayStation 1", "PlayStation 2", "PSP", "Nintendo 64",
+   "Nintendo DS"]
+
+
+
+}
