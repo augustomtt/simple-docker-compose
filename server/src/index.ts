d70abf827a1e8d19698fbbc6ca6a23f4f682b838
mostri28 <mateoetchepare@gmail.com>
2024-05-11 18:42:00 -0300

    routes server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,12 @@
 import express from 'express' 
 import cors from 'cors'
+import router from './routes/juego.routes';
 
 const app = express();
 const PORT = 3000;
 
 app.use(cors());
+app.use(router);
 
 app.listen(PORT, () => {
     console.log(`Corriendo en puerto ${PORT}`);
